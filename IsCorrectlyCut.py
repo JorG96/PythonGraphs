9d6dcb4e62cdd462cff9be526ea00eb94e92253d
JorG96 <jecamg96@gmail.com>
2021-11-28 11:15:15 -0400

    IsCorrectlyCut results added

--- IsCorrectlyCut.py
+++ IsCorrectlyCut.py
@@ -21,3 +21,14 @@ adj = [[false, true, false, false, false, true],
 the output should be
 solution(adj) = true.
 """
+def solution(adj):
+    allNeighbors = [set(j for j, b in enumerate(row) if b) for row in adj]
+    for neighbors in allNeighbors:
+        neighbors2 = set()
+        for n in neighbors:
+            neighbors2 |= allNeighbors[n]
+
+        if neighbors.intersection(neighbors2) or len(neighbors.union(neighbors2)) != len(adj) - 1:
+            return False
+        
+    return True
