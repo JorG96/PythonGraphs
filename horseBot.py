ac5586e91502f55e32b2eb647f901ced401afe1b
JorG96 <jecamg96@gmail.com>
2021-12-11 19:35:35 -0400

    horseBot example added

---
+++ horseBot.py
@@ -0,0 +1,14 @@
+"""
+Little Ratiorg was tired of being bullied by other bots and mighty CodeFighters, so he joined the Ninja Bots Training camp. Any bot that manages to solve all the challenges becomes an ultimate master of algorithms, and that is exactly what Ratiorg wants.
+
+The little bot has already successfully completed several challenges. Now he is facing a challenge in which he has to prove that he knows how to ride a horse and plan battles. Ratiorg is sitting on his digital horse in the top left corner of an n × m board. The horse can only make moves similar to those of a knight chess piece. Prior to any action, Ratiorg should define values a and b (0 < a ≤ b), which will specify the horse's moves: each move the horse will change one of its coordinates by a, and the other one by b.
+
+Ratiorg needs to define such a and b that will let him end up in the bottom right corner. How many ways are there to do this?
+
+Example
+
+For n = 3 and m = 3, the output should be
+solution(n, m) = 3.
+
+Ratiorg can define three pairs: (1, 1), (1, 2) and (2, 2)
+"""
