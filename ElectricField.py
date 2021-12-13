f1ecca6d2cba6e85e59d0203368a6edfbf237a6d
JorG96 <jecamg96@gmail.com>
2021-12-13 18:45:51 -0400

    ElectricField solution

--- ElectricField.py
+++ ElectricField.py
@@ -14,3 +14,56 @@ wires = [[1, 0, 1, 3], [3, 1, 3, 2], [4, 1, 4, 3], [4, 2, 4, 4],
 the output should be
 solution(grid, wires) = 26.
 """
+def solution(grid, wires):
+
+    G = dict()
+    for i in range(grid[1]):
+        for j in range(grid[0]):
+            G[(i,j)] = []
+            for x,y in [(i+1,j), (i-1,j), (i,j+1), (i, j-1)]:
+                if x >=0 and y>=0 and x < grid[1] and y < grid[0]:
+                    G[(i,j)].append((x,y))
+    for w in wires:
+        if w[0] == w[2]:
+            x = w[0]
+            lo, hi = min(w[1], w[3]), max(w[1], w[3])
+            for y in range(lo, hi):
+                a = (x,y)
+                b = (x-1, y)
+                if b in G.get(a,[]):
+                    G[a].remove(b)
+                if a in G.get(b,[]):
+                    G[b].remove(a)
+            
+        elif w[1] == w[3]:
+            y = w[1]
+            lo, hi = min(w[0], w[2]), max(w[0], w[2])
+            for x in range(lo, hi):
+                a = (x,y)
+                b = (x, y-1)
+                if b in G.get(a,[]):
+                    G[a].remove(b)
+                if a in G.get(b,[]):
+                    G[b].remove(a)
+    goal = (grid[1]-1, grid[0]-1)                
+    return dfs(G, goal)
+def dfs(G, end):
+    queue = [(0,0)]
+    
+    prev = {(0,0): None}
+    
+    while queue:
+        cur = queue.pop(0)
+        for n in G[cur]:
+            if n not in prev:
+                queue.append(n)
+                prev[n] = cur
+    if end not in prev:
+        return -1
+    path = []
+    cur = end
+    while cur:
+        path.append(cur)
+        cur = prev[cur]
+    print(path[::-1])
+    return len(path) -1
