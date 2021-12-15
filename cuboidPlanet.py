1629154d58283a593100f4d82703bf3f6d6b1ff1
JorG96 <jecamg96@gmail.com>
2021-12-14 23:03:05 -0400

    cuboidPlanet solution

--- cuboidPlanet.py
+++ cuboidPlanet.py
@@ -29,3 +29,148 @@ There are four areas in which all cells are connected. Thus, the number of pairs
 
 Thus, the answer is 136 - 30 = 106.
 """
+def solution(cuboid, impassableCells):
+    
+    import sys
+    sys.setrecursionlimit(10**6)
+    #Step 1, create separate area arrays and add obstructions
+    import numpy as np
+    a, b, c = cuboid
+    zero, one, two, three, four, five = np.ones((a, c)), np.ones((a, b)), np.ones((a, c)), np.ones((c, b)), np.ones((a, b)), np.ones((c, b))
+    
+    d = {0:zero, 1:one,  2:two, 3:three, 4:four,  5:five}
+    for area, x, y in impassableCells:
+        d[area][x, y] = 0
+        
+    
+    #2.  Create 6 different adjacency matrices
+    def adjacent(x, y, area, areaCode):
+        nr, nc = area.shape
+        all_moves = [(x - 1, y),(x + 1, y), (x, y + 1), (x, y - 1)]
+        return [(areaCode, a, b) for a, b in all_moves if 0 <= a < nr and 0 <= b < nc and area[a, b]]
+    
+    adj_lists = []
+    for a in d:
+        area = d[a]        
+        nr, nc = area.shape
+        adj_lists.append({(a, i, j): adjacent(i, j, area, a)for i in range(nr) for j in range(nc) if area[i, j]})
+        
+    
+    #3. Find connected components of these individual areas
+    
+    def dfs(s, adjList, cc):
+        if s not in cc:
+            cc.add(s)
+            for adj in adjList[s]:
+                dfs(adj, adjList, cc)    
+    
+    connectedComponents = {}
+    
+
+    #Merge adj_lists to create new one for the net
+    netAdjList = {cell:list(areaAdjList[cell]) for areaAdjList in adj_lists for cell in areaAdjList}
+    a, b, c = cuboid
+    
+    #Create links between areas 0, 1, and 2:
+    for r in range(a):
+        lkey, rkey = (0, r, c - 1), (1, r, 0)
+        if lkey in netAdjList and rkey in netAdjList:
+            netAdjList[lkey].append(rkey)
+            netAdjList[rkey].append(lkey)
+        
+        lkey, rkey = (1, r, b - 1), (2, r, 0)
+        if lkey in netAdjList and rkey in netAdjList:
+            netAdjList[lkey].append(rkey)
+            netAdjList[rkey].append(lkey)
+    
+    #Create link between 1 and 3, 3 and 4 and 4, 5    
+    
+    
+    for col in range(b):        
+        ukey, bkey = (1, a - 1, col), (3, 0, col)
+        if ukey in netAdjList and bkey in netAdjList:
+            netAdjList[ukey].append(bkey)
+            netAdjList[bkey].append(ukey)
+            
+        ukey, bkey = (3, c - 1, col), (4, 0, col)
+        if ukey in netAdjList and bkey in netAdjList:
+            netAdjList[ukey].append(bkey)
+            netAdjList[bkey].append(ukey)        
+    
+        ukey, bkey = (4, a - 1, col), (5, 0, col)        
+        if ukey in netAdjList and bkey in netAdjList:
+            netAdjList[ukey].append(bkey)
+            netAdjList[bkey].append(ukey)                
+    
+    
+    visited = set()
+    cc_lengths = []
+    for k in netAdjList:
+        if k not in visited:
+            cc = set()
+            dfs(k, netAdjList, cc)
+            cc_lengths.append(len(cc))            
+            visited.update(cc)
+
+    net_combinations = sum(map(lambda x: x * (x - 1) //2, cc_lengths))
+    
+    #Continue updating  the netAdjList to form the adj list for cube 
+    #Join bottom of 0 to left of 3 (reversed) and bottom of 2 to right of 3(reversed),
+    ## top of 0 to left of 5 and top of 2 to right of 5
+    for col in range(c):
+        lkey, rkey = (0, a - 1, col), (3, c - col - 1, 0)
+        if lkey in netAdjList and rkey in netAdjList:
+            netAdjList[lkey].append(rkey)
+            netAdjList[rkey].append(lkey)
+        
+        lkey, rkey = (3, col, b - 1), (2, a - 1, col)
+        if lkey in netAdjList and rkey in netAdjList:
+            netAdjList[lkey].append(rkey)
+            netAdjList[rkey].append(lkey)     
+        
+        lkey, rkey = (0, 0, col), (5, col, 0)
+        if lkey in netAdjList and rkey in netAdjList:
+            netAdjList[lkey].append(rkey)
+            netAdjList[rkey].append(lkey)
+            
+        lkey, rkey = (5, c - col - 1, b - 1), (2, 0, col)
+        if lkey in netAdjList and rkey in netAdjList:
+            netAdjList[lkey].append(rkey)
+            netAdjList[rkey].append(lkey)            
+            
+                
+    #connect left of 0 to left of 4 and right of 2 to right of 
+    for row in range(a):
+        lkey, rkey = (0, row, 0), (4, a - row - 1, 0)
+        if lkey in netAdjList and rkey in netAdjList:
+            netAdjList[lkey].append(rkey)
+            netAdjList[rkey].append(lkey)
+            
+        lkey, rkey = (4, a - row - 1, b - 1), (2, row, c - 1)
+        if lkey in netAdjList and rkey in netAdjList:
+            netAdjList[lkey].append(rkey)
+            netAdjList[rkey].append(lkey)        
+        
+    #Connect top of 1 to bottom of 5
+    for col in range(b):
+        lkey, rkey = (1, 0, col), (5, c - 1, col)        
+        if lkey in netAdjList and rkey in netAdjList:
+            netAdjList[lkey].append(rkey)
+            netAdjList[rkey].append(lkey)        
+    
+    #Find connected components again
+    visited = set()
+    cc_lengths = []
+    ccs = []
+    for k in netAdjList:
+        if k not in visited:
+            cc = set()
+            dfs(k, netAdjList, cc)
+            cc_lengths.append(len(cc))
+            if len(cc) == 1:
+                ccs.append(cc)
+            visited.update(cc)         
+            
+    cube_combinations = sum(map(lambda x: x * (x - 1) //2, cc_lengths))
+
+    return cube_combinations - net_combinations
