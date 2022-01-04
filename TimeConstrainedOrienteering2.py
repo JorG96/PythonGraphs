c2c40e3c0a485473fbf60efab7e3178f995a9745
JorG96 <jecamg96@gmail.com>
2022-01-04 10:54:29 -0400

    TimeConstrainedOrienteering2 solution

--- TimeConstrainedOrienteering2.py
+++ TimeConstrainedOrienteering2.py
@@ -20,3 +20,15 @@ solution(n, roads) = [[0, 1, 2],
                       [4, 0, 2],
                               
 """
+
+def solution(n, m):
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if 0 <= m[i][k] and 0 <= m[k][j]:
+                    if 0 <= m[i][j]:
+                        m[i][j] = min(m[i][j], max(m[i][k], m[k][j]))
+                    else:
+                        m[i][j] = max(m[i][j], m[i][k], m[k][j])
+                    
+    return m
