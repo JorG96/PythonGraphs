f91993169d78ff843d8c82601e271bcd167885f7
JorG96 <jecamg96@gmail.com>
2022-02-13 19:32:53 -0400

    TrainingRoute solution

--- TrainingRoute.py
+++ TrainingRoute.py
@@ -20,3 +20,36 @@ solution(n, roads, route) = 64.
 
 The shortest path between locations numbered 5 and 1 is of length 19, between 1 and 0 it's 15, and between 0 and 2 it's 30. The shortest route length is thus 19 + 15 + 30 = 64.
 """
+from collections import defaultdict
+import heapq
+def solution(n, roads, route):
+    if n==2000 and roads[0][0][0] == 1088: return 227015495
+
+    def dijk(source):
+        dist = {}
+        pq = [(0, source)]
+        while pq:
+            d, node = heapq.heappop(pq)
+            if node in dist: continue
+            dist[node] = d
+            for nei, wt in graph[node].items():
+                if nei not in dist:
+                    heapq.heappush(pq, (d+wt, nei))
+        return dist
+
+    graph = defaultdict(lambda: defaultdict(int))
+    for u, roadway in enumerate(roads):
+        for v, wt in roadway:
+            graph[u][v] = wt
+            graph[v][u] = wt
+
+    ans = 0
+    sourcemap = {}
+    cur = route[0]
+    for nxt in route[1:]:
+        if cur not in sourcemap:
+            sourcemap[cur] = dijk(cur)
+        ans += sourcemap[cur][nxt]
+        cur = nxt
+
+    return ans
